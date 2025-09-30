cbf52745243b9550dfe4464d0bab98d137f3d973
Tristan Spear <tspear1704@gmail.com>
2025-09-30 09:54:02 -0700

    Add form features

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,7 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 const characters = [
   {
@@ -22,24 +23,7 @@ const characters = [
 ];
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -54,6 +38,7 @@ function MyApp() {
       characterData={characters} 
       removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
   }
